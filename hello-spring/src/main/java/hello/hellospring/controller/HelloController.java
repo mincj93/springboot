dfe6a0c379b3bb7963067e3db94e31589274211f
mincj <mincj93@naver.com>
2023-06-30 14:05:32 +0900

    수정1

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -12,8 +12,9 @@ public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model){
+        int k = 20;
         int i = 10;
-        System.out.println(i);
+
         model.addAttribute("data", "hello~!");
         return "hello";
     };
@@ -21,7 +22,6 @@ public class HelloController {
     @GetMapping("hello2")
     public String hello2(Model model){
         int i = 123;
-        System.out.println(i);
         model.addAttribute("data", "hello22222~!");
         return "/folder1/hello2";
     };
